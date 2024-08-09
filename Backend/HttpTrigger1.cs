0272d1e252673c3c4db729c6882489f02dda7c89
Karim Vaes <github@kvaes.be>
2024-08-09 11:38:00 +0000

    we would never commit a secret

--- Backend/HttpTrigger1.cs
+++ Backend/HttpTrigger1.cs
@@ -18,6 +18,7 @@ namespace Security4PaasLovers.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string nosecret = "sp=r&st=2024-08-09T11:28:31Z&se=2024-08-09T19:28:31Z&spr=https&sv=2022-11-02&sr=c&sig=GnCtmi764jnR%2B09eBoEQ%2BRPNI0U5eaORZ9H4qO2QToc%3D";
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
